6b5f92447946bc7a1c4a75e59650019d1a5567d8
cattokomo <71205197+cattokomo@users.noreply.github.com>
2024-10-16 19:35:52 +0800

    fully complete json library

--- src/helpers.zig
+++ src/helpers.zig
@@ -12,3 +12,21 @@ pub inline fn throwZigError(L: *lunaro.State, err: anyerror) void {
     //     L.raise("function '%s' throws an error '%s'", .{ info.name, @errorName(err) });
     L.raise("function returns an error '%s'", .{@errorName(err)});
 }
+
+pub const Handler = struct {
+    handler: fn (ud: ?*anyopaque) void,
+    ud: ?*anyopaque,
+    L: *lunaro.State,
+
+    pub fn handle(self: Handler, ret: anytype) @TypeOf(ret) {
+        return ret catch |err| {
+            self.handler(self.ud);
+            throwZigError(self.L, err);
+            unreachable;
+        };
+    }
+};
+
+pub inline fn handleZigError(L: *lunaro.State, comptime handler: fn (ud: ?*anyopaque) void, ud: ?*anyopaque) Handler {
+    return .{ handler, ud, L };
+}
--- src/std/json.zig
+++ src/std/json.zig
@@ -4,6 +4,8 @@ const helpers = @import("../helpers.zig");
 
 const json = std.json;
 
+const json_array: [*]const u8 = undefined;
+
 pub fn open(L: *lunaro.State) c_int {
     L.createtable(0, 3);
     const table = lunaro.Table.init(L, L.gettop());
@@ -11,6 +13,15 @@ pub fn open(L: *lunaro.State) c_int {
 
     L.pushclosure(decode, 0);
     table.set("decode");
+
+    L.pushclosure(encode, 0);
+    table.set("encode");
+
+    L.pushlightuserdata(&json_array);
+    table.set("emptyarray");
+
+    L.pushlightuserdata(@as(?*anyopaque, @ptrFromInt(0)));
+    table.set("null");
 
     table.push(L);
     return 1;
@@ -24,23 +35,45 @@ fn decode(L: *lunaro.State) c_int {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const parsed = json.parseFromSlice(json.Value, allocator, str, .{}) catch |err| blk: {
+    const parsed = json.parseFromSlice(json.Value, allocator, str, .{}) catch |err| {
         _ = gpa.deinit();
         helpers.throwZigError(L, err);
-        break :blk json.Parsed(json.Value){};
+        unreachable;
     };
     defer parsed.deinit();
 
-    internalDecode(L, parsed.value);
+    internalDecode(L, parsed.value, allocator) catch |err| {
+        _ = gpa.deinit();
+        helpers.throwZigError(L, err);
+        unreachable;
+    };
     return 1;
 }
 
 fn encode(L: *lunaro.State) c_int {
-    _ = L;
-    return 0;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var list = std.ArrayList(u8).init(allocator);
+    defer list.deinit();
+    const out = list.writer();
+
+    var w = json.writeStream(out, .{});
+    defer w.deinit();
+
+    internalEncode(L, 1, @constCast(&w)) catch |err| {
+        _ = gpa.deinit();
+        w.deinit();
+        helpers.throwZigError(L, err);
+        unreachable;
+    };
+
+    L.pushstring(list.items);
+    return 1;
 }
 
-fn internalDecode(L: *lunaro.State, value: json.Value) void {
+fn internalDecode(L: *lunaro.State, value: json.Value, allocator: std.mem.Allocator) !void {
     switch (value) {
         .null => L.pushnil(),
         .bool => |v| L.pushboolean(v),
@@ -54,9 +87,9 @@ fn internalDecode(L: *lunaro.State, value: json.Value) void {
         },
         .string => |v| L.pushstring(v),
         .array => |v| {
-            L.createtable(0, 0);
+            L.createtable(@intCast(v.items.len), 0);
             for (v.items, 0..) |arr_v, i| {
-                internalDecode(L, arr_v);
+                try internalDecode(L, arr_v, allocator);
                 L.seti(-2, @intCast(i + 1));
             }
         },
@@ -64,21 +97,71 @@ fn internalDecode(L: *lunaro.State, value: json.Value) void {
             const keys = v.keys();
             const values = v.values();
 
-            var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-            defer _ = gpa.deinit();
-            const allocator = gpa.allocator();
-
-            L.createtable(0, 0);
+            L.createtable(0, @intCast(keys.len));
             for (keys, 0..) |k, i| {
-                internalDecode(L, values[i]);
-                const t_k = allocator.dupeZ(u8, k) catch |err| blk: {
-                    _ = gpa.deinit();
-                    helpers.throwZigError(L, err);
-                    break :blk "";
-                };
-                L.setfield(-2, t_k);
-                allocator.free(t_k);
+                try internalDecode(L, values[i], allocator);
+                const zero_k = try allocator.dupeZ(u8, k);
+                L.setfield(-2, zero_k);
+                allocator.free(zero_k);
             }
         },
     }
+}
+
+fn internalEncode(L: *lunaro.State, idx: lunaro.Index, w: anytype) !void {
+    return switch (L.typeof(idx)) {
+        .lightuserdata => blk: {
+            const value = L.touserdata([*]const u8, idx);
+            if (value == null) {
+                try w.write(null);
+                break :blk;
+            } else if (value == &json_array) {
+                try w.beginArray();
+                try w.endArray();
+                break :blk;
+            }
+            L.raise("cannot encode a value of light userdata!", .{});
+            unreachable;
+        },
+        .nil, .none => undefined,
+        .boolean => try w.write(L.toboolean(idx)),
+        .string => try w.write(L.tostring(idx).?),
+        .number => {
+            const num = L.tonumber(idx);
+            if (num == @round(num))
+                try w.write(@as(i64, @intFromFloat(@round(num))))
+            else
+                try w.write(num);
+        },
+        .table => {
+            L.len(idx);
+            const len = L.tointeger(-1);
+            L.pop(1);
+            if (len == 0) {
+                try w.beginObject();
+                L.pushnil();
+                while (L.next(idx)) {
+                    const key = L.tostring(-2).?;
+                    try w.objectField(key);
+                    try internalEncode(L, L.gettop(), w);
+                    L.pop(1);
+                }
+                try w.endObject();
+            } else {
+                try w.beginArray();
+                var i: i64 = 1;
+                while (i <= len) {
+                    _ = L.geti(idx, i);
+                    try internalEncode(L, L.gettop(), w);
+                    L.pop(1);
+                    i += 1;
+                }
+                try w.endArray();
+            }
+        },
+        else => {
+            L.raise("cannot encode a value of %s!", .{L.typenameof(idx)});
+            unreachable;
+        },
+    };
 }
